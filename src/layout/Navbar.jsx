05e67c290bcc44a13dfaf021f0b1b2d581c2aeca
EunseoKim <dmstj9408@gmail.com>
2024-09-11 22:43:26 +0900

    [feat] connect other pages in navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,19 +6,26 @@ import { Container } from 'react-bootstrap';
 const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const genres = ['연극', '뮤지컬', '콘서트', '무용(서양/한국무용)', '대중무용', '서양음악(클래식)', '한국음악(국악)', '서커스/마술'];
+  const genres_eng = ['theaters', 'musicals', 'concerts'];
   const navigate = useNavigate();
 
   const handleSearch = (e) => {
     e.preventDefault();
     // 검색 기능 로직 추가 가능
     console.log("검색어1:", searchTerm);
-    // navigate (`${searchTerm}`)
+    navigate (`${searchTerm}`)
   };
   return (
     <Container>
       <div className='nav-top'>
         <div className='nav-logo-search-area'>
-          <img width='200px' src='https://www.creativefabrica.com/wp-content/uploads/2020/04/12/Ticket-Logo-Graphics-3851465-1.jpg'  alt="profile"/>
+          <a className="nav-logo" href="/">
+            <img 
+              width='200px' 
+              src='https://www.creativefabrica.com/wp-content/uploads/2020/04/12/Ticket-Logo-Graphics-3851465-1.jpg'  
+              alt="logo"
+            />
+          </a>
           <form  onSubmit={handleSearch}>
             <div className='nav-input-box'>
               <input
@@ -38,18 +45,18 @@ const Navbar = () => {
           </form>
         </div>  
         <div className='nav-top-items'>
-          <a href='#!'>로그인</a>
+          <a href='login'>로그인</a>
           <a href='#!'>회원가입</a>
-          <a href='#!'>마이페이지</a>
+          <a href='my-select'>마이페이지</a>
         </div>
       </div>
       <div className='nav-bottom'>
         {genres.map((genre,idx) => (
-          <a href='#!' key={idx}>{genre}</a>
+          <a href={genres_eng[idx]?genres_eng[idx]:'#!'} key={idx}>{genre}</a>
         ))}
         <a className='text-gray'>  |  </a>
-        <a className='main-color'>랭킹</a>
-        <a className='main-color'>지역별</a>
+        <a href='rankings' className='main-color'>랭킹</a>
+        <a href='regions' className='main-color'>지역별</a>
       </div>
     </Container>
   )
