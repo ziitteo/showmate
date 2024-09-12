285050d3574bf86227339aa72dfe78cd3b118996
Rosie Seok <lovelyrosie731@gmail.com>
2024-09-12 21:45:23 +0900

    [Feat] .env

---
+++ src/pages/MusicalDetail/MusicalDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MusicalDetailPage = () => {
+  return <div></div>;
+};
+
+export default MusicalDetailPage;
---
+++ src/pages/TheaterDetail/TheaterDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TheaterDetailPage = () => {
+  return <div></div>;
+};
+
+export default TheaterDetailPage;
