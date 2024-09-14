e763893136675bf937304e6b042e7d0f9002403e
Rosie Seok <lovelyrosie731@gmail.com>
2024-09-14 14:26:25 +0900

    [feat, style] add useSearch.js and SearchResultPage.jsx and Style.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import AppLayout from './layout/AppLayout';
-import Homepage from './pages/Homepage/Homepage';
-import MusicalPage from './pages/Musical/MusicalPage';
-import PlayPage from './pages/Play/PlayPage';
-import ConcertPage from './pages/Concert/ConcertPage';
-import DetailPage from './pages/DetailPage/DetailPage';
-import RegionPage from './pages/Region/RegionPage';
-import RankingPage from './pages/Ranking/RankingPage';
-import MySelectPage from './pages/MySelect/MySelectPage';
-import DancePage from './pages/Dance/DancePage';
-import CircusMagicPage from './pages/CircusMagic/CircusMagicPage';
-import CompositePage from './pages/Composite/CompositePage';
-import GugakPage from './pages/Gugak/GugakPage';
-import PublicDancePage from './pages/PublicDance/PublicDancePage';
-import ClassicPage from './pages/Classic/ClassicPage';
-import Login from './pages/Login/Login';
-import { GlobalProvider } from './pages/DetailPage/GlobalContext';
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import Homepage from "./pages/Homepage/Homepage";
+import MusicalPage from "./pages/Musical/MusicalPage";
+import PlayPage from "./pages/Play/PlayPage";
+import ConcertPage from "./pages/Concert/ConcertPage";
+import DetailPage from "./pages/DetailPage/DetailPage";
+import RegionPage from "./pages/Region/RegionPage";
+import RankingPage from "./pages/Ranking/RankingPage";
+import MySelectPage from "./pages/MySelect/MySelectPage";
+import DancePage from "./pages/Dance/DancePage";
+import CircusMagicPage from "./pages/CircusMagic/CircusMagicPage";
+import CompositePage from "./pages/Composite/CompositePage";
+import GugakPage from "./pages/Gugak/GugakPage";
+import PublicDancePage from "./pages/PublicDance/PublicDancePage";
+import ClassicPage from "./pages/Classic/ClassicPage";
+import Login from "./pages/Login/Login";
+import SearchResultsPage from "./pages/SearchResults/SearchResultsPage"; // 새로 추가된 검색 결과 페이지
+import { GlobalProvider } from "./pages/DetailPage/GlobalContext";
 // 홈페이지
 // 뮤지컬 전체 페이지 /musicals
 // 연극 전체 페이지 /theaters
@@ -31,28 +32,32 @@ function App() {
   const [authenticate, setAuthenticate] = useState(false); // 로그인 성공 유무
 
   useEffect(() => {
-    console.log('Check Login :', authenticate);
+    console.log("Check Login :", authenticate);
   }, [authenticate]);
 
   return (
     <GlobalProvider>
       <Routes>
-        <Route path='/' element={<AppLayout />}>
+        <Route path="/" element={<AppLayout />}>
           <Route index element={<Homepage />} />
-          <Route path='/login' element={<Login setAuthenticate={setAuthenticate} />} />
-          <Route path='/play' element={<PlayPage />} />
-          <Route path='/musical' element={<MusicalPage />} />
-          <Route path='/concert' element={<ConcertPage />} />
-          <Route path='/dance' element={<DancePage />} />
-          <Route path='/publicdance' element={<PublicDancePage />} />
-          <Route path='/classic' element={<ClassicPage />} />
-          <Route path='/gugak' element={<GugakPage />} />
-          <Route path='/circusmagic' element={<CircusMagicPage />} />
-          <Route path='/composite' element={<CompositePage />} />
-          <Route path='/goods/:id' element={<DetailPage />} />
-          <Route path='/region' element={<RegionPage />} />
-          <Route path='/ranking' element={<RankingPage />} />
-          <Route path='/my-select' element={<MySelectPage />} />
+          <Route
+            path="/login"
+            element={<Login setAuthenticate={setAuthenticate} />}
+          />
+          <Route path="/play" element={<PlayPage />} />
+          <Route path="/musical" element={<MusicalPage />} />
+          <Route path="/concert" element={<ConcertPage />} />
+          <Route path="/dance" element={<DancePage />} />
+          <Route path="/publicdance" element={<PublicDancePage />} />
+          <Route path="/classic" element={<ClassicPage />} />
+          <Route path="/gugak" element={<GugakPage />} />
+          <Route path="/circusmagic" element={<CircusMagicPage />} />
+          <Route path="/composite" element={<CompositePage />} />
+          <Route path="/goods/:id" element={<DetailPage />} />
+          <Route path="/region" element={<RegionPage />} />
+          <Route path="/ranking" element={<RankingPage />} />
+          <Route path="/my-select" element={<MySelectPage />} />
+          <Route path="/search" element={<SearchResultsPage />} />
         </Route>
       </Routes>
     </GlobalProvider>
---
+++ src/hooks/useSearch.js
@@ -0,0 +1,39 @@
+// useSearchQuery.js
+import { useQuery } from "@tanstack/react-query";
+import fetchData from "../utils/api";
+
+const useSearchQuery = (
+  searchTerm,
+  page = 1,
+  genre = "",
+  saleStatus = "",
+  regionCode = "",
+  params = {}
+) => {
+  // 검색어가 공란일 때 전체 데이터를 가져오도록 기본 파라미터 설정
+  const defaultParams = {
+    rows: 10, // 한 번에 가져올 데이터 수 (페이지당 아이템 수)
+    ...(searchTerm ? { shprfnm: searchTerm } : {}), // 공연 이름에 검색어 포함, 검색어가 없으면 전체 리스트
+    ...(genre ? { shcate: genre } : {}), // 장르가 선택되었을 경우 필터링 추가
+    ...(saleStatus ? { prfstate: saleStatus } : {}), // 판매 상태가 선택되었을 경우 필터링 추가
+    ...(regionCode ? { signgucode: regionCode } : {}), // 지역 코드가 선택되었을 경우 필터링 추가
+    cpage: page, // 현재 페이지 번호
+    ...params,
+  };
+
+  // useQuery로 검색 데이터를 가져오는 로직 구현
+  return useQuery({
+    queryKey: ["search", defaultParams], // 쿼리 키 설정
+
+    // API 요청 함수 정의
+    queryFn: async () => {
+      const result = await fetchData("/pblprfr", defaultParams);
+      return result;
+    },
+
+    // 필요한 데이터 선택
+    select: (data) => data.dbs?.db || [],
+  });
+};
+
+export default useSearchQuery;
