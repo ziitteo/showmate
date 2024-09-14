12b638bc2ad76970571c2d0a45ae47a2080d3e7c
jy01.lee <jy01.lee@smarterbuild.net>
2024-09-14 15:39:32 +0900

    [feat] Login Features - "로그인" when the status of logging in to Navbar is true, and "Logout" when false - Create a PrivateRoute to enable access only if login status is true when accessing other pages

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,67 +1,99 @@
-import React, { useState } from 'react';
-import './Navbar.style.css';
-import { useNavigate } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import React, { useState } from "react";
+import "./Navbar.style.css";
+import { useNavigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
-const Navbar = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const genres = ['연극', '뮤지컬', '콘서트', '서양/한국무용', '대중무용', '클래식', '국악', '서커스/마술','복합예술공연'];
-  const genres_eng = ['play', 'musical', 'concert', 'dance', 'publicdance', 'classic', 'gugak', 'circusmagic', 'composite'];
+const Navbar = ({ authenticate, setAuthenticate }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const genres = [
+    "연극",
+    "뮤지컬",
+    "콘서트",
+    "서양/한국무용",
+    "대중무용",
+    "클래식",
+    "국악",
+    "서커스/마술",
+    "복합예술공연",
+  ];
+  const genres_eng = [
+    "play",
+    "musical",
+    "concert",
+    "dance",
+    "publicdance",
+    "classic",
+    "gugak",
+    "circusmagic",
+    "composite",
+  ];
 
   const navigate = useNavigate();
 
-  const handleSearch = e => {
+  const handleSearch = (e) => {
     e.preventDefault();
     // 검색 기능 로직 추가 가능
-    console.log('검색어1:', searchTerm);
+    console.log("검색어1:", searchTerm);
     navigate(`${searchTerm}`);
   };
+
+  const handleLogout = () => {
+    setAuthenticate(false); // 로그아웃 처리
+    navigate("/");
+  };
+
   return (
     <Container>
-      <div className='nav-top'>
-        <div className='nav-logo-search-area'>
-          <a className='nav-logo' href='/'>
+      <div className="nav-top">
+        <div className="nav-logo-search-area">
+          <a className="nav-logo" href="/">
             <img
-              width='200px'
-              src='https://www.creativefabrica.com/wp-content/uploads/2020/04/12/Ticket-Logo-Graphics-3851465-1.jpg'
-              alt='logo'
+              width="200px"
+              src="https://www.creativefabrica.com/wp-content/uploads/2020/04/12/Ticket-Logo-Graphics-3851465-1.jpg"
+              alt="logo"
             />
           </a>
           <form onSubmit={handleSearch}>
-            <div className='nav-input-box'>
+            <div className="nav-input-box">
               <input
-                type='text'
-                placeholder='검색어를 입력하세요'
+                type="text"
+                placeholder="검색어를 입력하세요"
                 value={searchTerm}
-                onChange={event => setSearchTerm(event.target.value)}
+                onChange={(event) => setSearchTerm(event.target.value)}
               />
               <img
-                width='50px'
-                src='https://static.vecteezy.com/system/resources/previews/009/652/218/non_2x/magnifying-glass-icon-isolated-on-white-background-search-illustration-vector.jpg'
-                alt='search-icon'
-                className='nav-search-icon'
+                width="50px"
+                src="https://static.vecteezy.com/system/resources/previews/009/652/218/non_2x/magnifying-glass-icon-isolated-on-white-background-search-illustration-vector.jpg"
+                alt="search-icon"
+                className="nav-search-icon"
                 onClick={handleSearch}
               />
             </div>
           </form>
         </div>
-        <div className='nav-top-items'>
-          <a href='login'>로그인</a>
-          <a href='#!'>회원가입</a>
-          <a href='my-select'>내가 찜한 리스트</a>
+        <div className="nav-top-items">
+          {authenticate ? (
+            <a href="#" onClick={handleLogout}>
+              로그아웃
+            </a>
+          ) : (
+            <a href="/login">로그인</a>
+          )}
+          <a href="#!">회원가입</a>
+          <a href="my-select">내가 찜한 리스트</a>
         </div>
       </div>
-      <div className='nav-bottom'>
+      <div className="nav-bottom">
         {genres.map((genre, idx) => (
-          <a href={genres_eng[idx] ? genres_eng[idx] : '#!'} key={idx}>
+          <a href={genres_eng[idx] ? genres_eng[idx] : "#!"} key={idx}>
             {genre}
           </a>
         ))}
-        <a className='text-gray'> | </a>
-        <a href='ranking' className='main-color'>
+        <a className="text-gray"> | </a>
+        <a href="ranking" className="main-color">
           랭킹
         </a>
-        <a href='region' className='main-color'>
+        <a href="region" className="main-color">
           지역별
         </a>
       </div>
