0766dca48c9b68d55cfd942d0e8d000b1dd9f2bc
EunseoKim <dmstj9408@gmail.com>
2024-09-14 20:12:02 +0900

    [fix] fix deploy error in detail page

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,21 +1,20 @@
 import axios from 'axios';
 
 // 환경 변수에서 API 키 가져오기
-// const API_KEY = process.env.REACT_APP_API_KEY;
-const API_KEY = 'bf12e40f012c4d3d986da6cb893a26b7';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 // axios 인스턴스 생성
 const instance = axios.create({
-  baseURL: '/api/openApi/restful', // 프록시 경로를 통해 API 요청
+  // 배포,로컬 환경별로 프록시 경로 설정
+  baseURL: process.env.NODE_ENV === 'production' 
+    ? '/api'                   // production(배포 환경)에서는 '/api'로 설정
+    : '/api/openApi/restful',  // development(로컬 환경)에서는 '/api/openApi/restful'로 설정
   headers: {
     Accept: 'application/xml', // XML 응답 수락
-    // CORS(Cross-Origin Resource Sharing) 정책을 우회하기 위해 모든 출처(origin)에서의 요청을 허용합니다. 이렇게 하면 브라우저에서의 클라이언트 사이드 호출에 제한을 두지 않습니다. 이 헤더를 서버 측에서 설정해야 실제로 CORS를 허용할 수 있다.
-    'Access-Control-Allow-Origin': '*',
   },
   params: {
     service: API_KEY
   }
 });
-console.log('instance', instance)
 
 export default instance;
